5348f685b94c9a0284bf84ac8e88821def5c07fc
ranggakrisnaa <infinity.duos@gmail.com>
2024-01-03 17:55:47 +0700

    first commit

---
+++ models/product.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.hasMany(models.User)
+    }
+  }
+  Product.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    SKU: DataTypes.STRING,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
