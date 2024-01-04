810bfed79957ed2b6858c8ea47f236844e24bfe1
ranggakrisnaa <infinity.duos@gmail.com>
2024-01-04 10:56:17 +0700

    delete middleware routes

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -4,7 +4,6 @@ const AuthMiddleware = require('../middlewares/AuthMiddleware')
 const router = express.Router()
 
 router.post('/login', AuthController.login)
-router.use(AuthMiddleware.auth)
 router.post('/register', AuthController.register)
 
 module.exports = router
